78a8ba8546ab0c6b15eabb6f13055784c72e3f32
Nucklehead6478 <nucklehead6478@aol.com>
2025-04-12 19:45:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-const codes = {
-  1: "bunny",
-  2: "basket",
-  3: "egg",
-  4: "spring",
-  5: "jellybean",
-  6: "carrot",
-  7: "hop",
-  8: "chick",
-  9: "pastel",
-  10: "tulip",
-  11: "peeps",
-  12: "nest",
-  13: "petal",
-  14: "sunny",
-  15: "grass",
-  16: "surprise",
-  17: "hunt",
-  18: "ribbon",
-  19: "marshmallow",
-  20: "treat",
-  21: "delivery"
-};
+const clues = [
+  { password: "bunny", image: "images/image1.jpg" },
+  { password: "chocolate", image: "images/image2.jpg" },
+  { password: "jellybean", image: "images/image3.jpg" },
+  { password: "carrot", image: "images/image4.jpg" },
+  { password: "tulip", image: "images/image5.jpg" },
+  { password: "peeps", image: "images/image6.jpg" },
+  { password: "basket", image: "images/image7.jpg" },
+  { password: "daffodil", image: "images/image8.jpg" },
+  { password: "marshmallow", image: "images/image9.jpg" },
+  { password: "egg", image: "images/image10.jpg" },
+  { password: "bonnet", image: "images/image11.jpg" },
+  { password: "chick", image: "images/image12.jpg" },
+  { password: "spring", image: "images/image13.jpg" },
+  { password: "candy", image: "images/image14.jpg" },
+  { password: "lily", image: "images/image15.jpg" },
+  { password: "hunt", image: "images/image16.jpg" },
+  { password: "treats", image: "images/image17.jpg" },
+  { password: "flower", image: "images/image18.jpg" },
+  { password: "grass", image: "images/image19.jpg" },
+  { password: "sunshine", image: "images/image20.jpg" },
+  { password: "surprise", image: "images/image21.jpg" }
+];
 
-function checkCode(step) {
-  const input = document.getElementById('code' + step).value.trim().toLowerCase();
-  if (input === codes[step]) {
-    const nextImage = document.getElementById('img' + step);
-    if (nextImage) nextImage.style.display = 'block';
+let currentClueIndex = 0;
+
+function showClue() {
+  const clueContainer = document.getElementById("clueContainer");
+  if (currentClueIndex < clues.length) {
+    const clue = clues[currentClueIndex];
+    clueContainer.innerHTML = `
+      <img src="${clue.image}" alt="Clue ${currentClueIndex + 1}" style="max-width: 100%; margin-bottom: 10px;" />
+      <form onsubmit="return checkPassword(event)">
+        <input type="text" id="passwordInput" placeholder="Enter code for Egg #${currentClueIndex + 2}" />
+        <button type="submit">Submit</button>
+      </form>
+    `;
   } else {
-    alert("Oops! That's not the right code. Try again!");
+    clueContainer.innerHTML = "<h2>🎉 You found your Easter basket! 🎉</h2>";
   }
 }
+
+function checkPassword(event) {
+  event.preventDefault();
+  const input = document.getElementById("passwordInput").value.trim().toLowerCase();
+  if (input === clues[currentClueIndex].password) {
+    currentClueIndex++;
+    showClue();
+  } else {
+    alert("Oops! Try again 🐰");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", showClue);
