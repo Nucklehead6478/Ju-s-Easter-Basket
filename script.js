a3e676f18373b663c10e05dbfd297d746df02aeb
Nucklehead6478 <nucklehead6478@aol.com>
2025-04-12 20:22:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
 const clues = [
   { password: "bunny", image: "images/image1.jpg" },
-  { password: "carrot", image: "images/image2.jpg" },
-  { password: "basket", image: "images/image3.jpg" },
-  { password: "jellybean", image: "images/image4.jpg" },
-  { password: "chick", image: "images/image5.jpg" },
+  { password: "chocolate", image: "images/image2.jpg" },
+  { password: "jellybean", image: "images/image3.jpg" },
+  { password: "carrot", image: "images/image4.jpg" },
+  { password: "tulip", image: "images/image5.jpg" },
   { password: "peeps", image: "images/image6.jpg" },
-  { password: "daffodil", image: "images/image7.jpg" },
-  { password: "sunshine", image: "images/image8.jpg" },
-  { password: "pastel", image: "images/image9.jpg" },
-  { password: "marshmallow", image: "images/image10.jpg" },
-  { password: "sprinkles", image: "images/image11.jpg" },
-  { password: "tulip", image: "images/image12.jpg" },
-  { password: "chocolate", image: "images/image13.jpg" },
-  { password: "hatch", image: "images/image14.jpg" },
-  { password: "hop", image: "images/image15.jpg" },
-  { password: "puzzle", image: "images/image16.jpg" },
-  { password: "candy", image: "images/image17.jpg" },
-  { password: "duckling", image: "images/image18.jpg" },
-  { password: "petal", image: "images/image19.jpg" },
-  { password: "daisy", image: "images/image20.jpg" },
+  { password: "basket", image: "images/image7.jpg" },
+  { password: "daffodil", image: "images/image8.jpg" },
+  { password: "marshmallow", image: "images/image9.jpg" },
+  { password: "egg", image: "images/image10.jpg" },
+  { password: "bonnet", image: "images/image11.jpg" },
+  { password: "chick", image: "images/image12.jpg" },
+  { password: "spring", image: "images/image13.jpg" },
+  { password: "candy", image: "images/image14.jpg" },
+  { password: "lily", image: "images/image15.jpg" },
+  { password: "hunt", image: "images/image16.jpg" },
+  { password: "treats", image: "images/image17.jpg" },
+  { password: "flower", image: "images/image18.jpg" },
+  { password: "grass", image: "images/image19.jpg" },
+  { password: "sunshine", image: "images/image20.jpg" },
   { password: "surprise", image: "images/image21.jpg" }
 ];
 
-let currentClue = 0;
+let currentClueIndex = 0;
 
 function showClue() {
   const clueContainer = document.getElementById("clueContainer");
-  const passwordInput = document.getElementById("passwordInput");
-  const password = passwordInput.value.trim().toLowerCase();
-
-  if (password === clues[currentClue].password) {
-    currentClue++;
-    passwordInput.value = "";
-
-    if (currentClue < clues.length) {
-      clueContainer.innerHTML = `
-        <h2>Clue ${currentClue + 1}</h2>
-        <img src="${clues[currentClue].image}" alt="Clue ${currentClue + 1}" />
-      `;
-    } else {
-      // Final clue reached!
-      clueContainer.innerHTML = `
-        <h2>🎉 You found your Easter basket! 🎉</h2>
-        <img src="images/easter-bunny.gif" alt="Easter Bunny" style="width: 200px; margin: 20px auto; display: block; animation: bounce 1.5s infinite;" />
-        <img src="${clues[clues.length - 1].image}" alt="Final Clue" style="max-width: 100%; margin-top: 10px;" />
-      `;
-    }
+  if (currentClueIndex < clues.length) {
+    const clue = clues[currentClueIndex];
+    clueContainer.innerHTML = `
+      <img src="${clue.image}" alt="Clue ${currentClueIndex + 1}" style="max-width: 100%; margin-bottom: 10px;" />
+      <form onsubmit="return checkPassword(event)">
+        <input type="text" id="passwordInput" placeholder="Enter code for Egg #${currentClueIndex + 2}" />
+        <button type="submit">Submit</button>
+      </form>
+    `;
   } else {
-    alert("Hmm… that password doesn’t match this egg. Try again! 🧐");
+    clueContainer.innerHTML = "<h2>🎉 You found your Easter basket! 🎉</h2>";
   }
 }
 
-document.getElementById("submitBtn").addEventListener("click", showClue);
-
-// Optional: Allow Enter key to submit
-document.getElementById("passwordInput").addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
+function checkPassword(event) {
+  event.preventDefault();
+  const input = document.getElementById("passwordInput").value.trim().toLowerCase();
+  if (input === clues[currentClueIndex].password) {
+    currentClueIndex++;
     showClue();
+  } else {
+    alert("Oops! Try again 🐰");
   }
-});
+}
+
+document.addEventListener("DOMContentLoaded", showClue);
